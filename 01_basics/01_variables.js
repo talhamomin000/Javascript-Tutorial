fc06efb4bc8c96cb5614aa45b450544ba92c72b7
Talha Momin <121718008+talhamomin000@users.noreply.github.com>
2024-12-02 13:39:42 +0000

    variables and datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 123
+
+let accountEmail = "sample@120gmail.com"
+
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+
+// accountID = 213
+
+accountEmail = "sample120@gmail.com"
+accountPassword = "43210"
+accountCity = "Mumbai"
+
+console.table([accountID,accountEmail,accountPassword, accountCity])
+
+// console.log(accountID);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,56 @@
+"use strict"; // treal all js code as newer version
+
+// alert("hello")
+
+let name = "talha"   //string
+
+let age = 22         // number
+
+let isLoggedIn = false   // boolean
+
+
+/* datatypes:- 
+        Number
+        BigInt
+        String
+        Boolean
+        null 
+        undefined - the value can be define later
+        symbol - use for uniqueness
+
+
+
+
+*/ 
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+// Data type conversion
+
+let marks = 97;
+
+console.log(typeof(marks))
+console.log(marks)
+
+let marksInString = String(marks)
+console.log(typeof(marksInString))
+console.log(marksInString)
+
+let password = undefined
+console.log(password)
+console.log(typeof(password))
+
+let passInString = Number(password)
+console.log(passInString)
+console.log(typeof(passInString))
+
+let isBool = true
+console.log(typeof(isBool))
+console.log(isBool)
+
+let isBoolNum = Number(isBool)
+console.log(isBoolNum)
+console.log(typeof(isBoolNum))
+
+
