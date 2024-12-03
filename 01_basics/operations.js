910e7fc9690648fc042fdce5573dfccee0c725cf
Talha Momin <121718008+talhamomin000@users.noreply.github.com>
2024-12-03 17:11:43 +0000

    operations in JS done

---
+++ 01_basics/operations.js
@@ -0,0 +1,41 @@
+console.log(2+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+
+let str1 = "talha"
+let str2 = "momin"
+let str3 = str1+str2
+console.log(str3)
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(2+2+"1")
+
+console.log(+true)  // gives output as 1
+
+
+//comparison operator
+
+console.log(2>2)
+console.log(2<2)
+console.log(2==2)
+console.log(2>=2)
+console.log(2<=2)
+console.log(2!=2)
+
+console.log("2">1) //javascript automatics converts string into number
+
+
+// Data Types Summary
+//There are two type
+
+// 1- Primitive - Number, string, boolean, null, undefined, symbol,Bigint
+
+// 2- Non Primitive/Reference - Array, Objects, functions
+
+
+
